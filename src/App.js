b6ae8441eaa4a82f778f637bf1d0c2c0753bfb7c
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-01-17 20:05:49 -0600

    background color #939979

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Change the background color.
         </p>
         <a
           className="App-link"
